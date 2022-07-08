fbbb9e7a58e7a728b2111881671576cbf7b06579
Killer Boss Original <73131550+killerbossoriginal@users.noreply.github.com>
2022-07-08 19:50:11 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
 
+var ciccio = {
+  baudo = 1,
+  pippo = 'amico di topolino'
+}
+
 async function get (auth, name) {
     await axios.get('http://greatnetwork.eu:10009/api/fius2022./database/bello/set/ciao')
     .then(function (response) {
@@ -16,3 +21,4 @@ async function get (auth, name) {
 }
 
 get()
+
